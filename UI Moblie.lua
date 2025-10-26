e6dd54b795915cdf99e52286bec69667a6bca9c9
BenJaMinZ <perkungeiei1@gmail.com>
2025-10-26 23:47:38 +0700

    Update UI Moblie.lua

--- UI Moblie.lua
+++ UI Moblie.lua
@@ -3,6 +3,7 @@ local VirtualInputManager = game:GetService("VirtualInputManager")
 local UserInputService = game:GetService("UserInputService")
 local CoreGui = game:GetService("CoreGui")
 local RunService = game:GetService("RunService")
+
 local player = Players.LocalPlayer
 
 local toggleGui = Instance.new("ScreenGui")
@@ -11,7 +12,6 @@ toggleGui.Parent = CoreGui
 
 local toggleButton = Instance.new("TextButton")
 toggleButton.Size = UDim2.new(0, 60, 0, 20)
-toggleButton.AnchorPoint = Vector2.new(0.5, 0.5)
 toggleButton.Position = UDim2.new(0.5, 0, 0.5, 0)
 toggleButton.Text = "Open UI"
 toggleButton.BackgroundColor3 = Color3.fromRGB(50, 50, 50)
@@ -19,35 +19,30 @@ toggleButton.TextColor3 = Color3.fromRGB(255, 255, 255)
 toggleButton.Parent = toggleGui
 toggleButton.AutoButtonColor = false
 
--- สถานะเปิด/ปิด UI
 local isOpen = false
 
--- ฟังก์ชันกด Ctrl ขวา
 local function pressCtrlRight()
     VirtualInputManager:SendKeyEvent(true, Enum.KeyCode.RightControl, false, game)
     VirtualInputManager:SendKeyEvent(false, Enum.KeyCode.RightControl, false, game)
 end
 
--- เมื่อกดปุ่ม
-toggleButton.MouseButton1Click:Connect(function()
-    isOpen = not isOpen
-    if isOpen then
-        toggleButton.Text = "Close UI"
-    else
-        toggleButton.Text = "Open UI"
-    end
-    pressCtrlRight()
-end)
-
 local dragging = false
 local offset = Vector2.new(0,0)
+local justDragged = false
 
 toggleButton.InputBegan:Connect(function(input)
     if input.UserInputType == Enum.UserInputType.MouseButton1 then
         dragging = true
+        justDragged = false
         local mousePos = UserInputService:GetMouseLocation()
         local guiPos = toggleButton.AbsolutePosition
         offset = mousePos - Vector2.new(guiPos.X, guiPos.Y)
+    end
+end)
+
+toggleButton.InputChanged:Connect(function(input)
+    if input.UserInputType == Enum.UserInputType.MouseMovement and dragging then
+        justDragged = true
     end
 end)
 
@@ -55,6 +50,17 @@ toggleButton.InputEnded:Connect(function(input)
     if input.UserInputType == Enum.UserInputType.MouseButton1 then
         dragging = false
     end
+end)
+
+toggleButton.MouseButton1Click:Connect(function()
+    if justDragged then return end
+    isOpen = not isOpen
+    if isOpen then
+        toggleButton.Text = "Close UI"
+    else
+        toggleButton.Text = "Open UI"
+    end
+    pressCtrlRight()
 end)
 
 RunService.RenderStepped:Connect(function()
